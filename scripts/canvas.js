37d44ac5e03fda0a80d540c4a5a36e40bbde73e4
Michael Ball <cycomachead@gmail.com>
2014-09-10 23:44:14 -0700

    more bug fixes for number splitting...I think

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -88,7 +88,13 @@ module.exports = function(robot) {
         var labNo  = msg.match[2];
         // match[3] is the late parameter.
         var points = msg.match[3] != undefined ? 1 : 2;
-        var SIDs   = msg.match[4].trim().split(/s+/);
+        var SIDs   = msg.match[4].trim().split(' ');
+        
+        // Trim spaces
+        var len = SIDs.length, i = 0;
+        for (; i < len; i += 1) {
+            SIDs[i] = SIDs[i].trim();
+        }
 
         msg.send('Checking Off ' + SIDs.length + ' students for lab ' + labNo + '.');
 
