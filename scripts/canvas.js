c7d8a13ffefd7aeb5bda628f834e63da021cde3a
Michael Ball <cycomachead@gmail.com>
2015-01-04 03:45:13 -0800

    a fragile server for slip days exists

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -79,6 +79,10 @@ var getAllLabs = function(courseID, assnGroupID, callback) {
     });
 };
 
+
+var pageSource = '<!DOCTYPE html><html><head><meta charset="utf-8"><title>Slip Day Checker</title><style type="text/css">body {background: #d3d6d9;color: #636c75;text-shadow: 0 1px 1px rgba(255, 255, 255, .5);font-family: Helvetica, Arial, sans-serif;}h1 {margin: 8px 0;padding: 0;}.commands {font-size: 13px;}p {border-bottom: 1px solid #eee;margin: 6px 0 0 0;padding-bottom: 5px;}p:last-child {border: 0;}</style></head><body><h1>#{SID}\'s Slip Day Check</h1><div class="commands">#{NOTES}</div></body></html>'
+
+
 module.exports = function(robot) {
 
     if (!authToken) {
@@ -226,9 +230,44 @@ module.exports = function(robot) {
             return;
         }
 
-        // Do the dirty work...
-        calculateSlipDays(student, msg);
+        calculateSlipDays(student, function(notices) {
+            notices.forEach(function(note) {
+                msg.send(note);
+            });
+        });
     });
+
+    robot.router.get('/slipdays/:sid', function(req, res) {
+        var sid = req.params.sid,
+            page = pageSource;
+
+        res.setHeader('content-type', 'text/html');
+        if (!sid) {
+            res.end(page.replace('#{NOTES}', 'No SID Found')
+                    .replace("#{SID}'s", "ERROR —"));
+            return;
+        }
+
+        calculateSlipDays(sid, function(notices) {
+            page = page.replace('#{SID}', sid);
+            results = notices.join('</p><p>')
+            results = '<p>' + results + '</p>';
+            res.end(page.replace('#{NOTES}', results))
+        });
+    });
+
+
+    /*
+    cmds = robot.helpCommands().map (cmd) ->
+      cmd.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;')
+
+    emit = "<p>#{cmds.join '</p><p>'}</p>"
+
+    emit = emit.replace /hubot/ig, "<b>#{robot.name}</b>"
+
+    res.setHeader 'content-type', 'text/html'
+    res.end helpContents robot.name, emit
+    */
 };
 
 ////
@@ -291,7 +330,7 @@ function getSlipDays(submissionTime, dueTime) {
  * [BASE]/courses/ID/students/submissions ?
  * Query: assignment_ids[]=XXX&student_ids[]=XXX&grouped=true&include=assignment
  */
-function calculateSlipDays(sid, msg) {
+function calculateSlipDays(sid, callback) {
     var url, query, idType = 'sis_user_id:';
 
     // Extension Student ID problems....
@@ -312,39 +351,35 @@ function calculateSlipDays(sid, msg) {
     // Include the student ID to query
     query += '&student_ids[]=' + idType + sid;
 
+    var results = [];
     cs10.get(url + query, '', function(body, response, errors) {
         // See above comments for API result formats
         var submissions, days, daysUsed;
         if (!body || body.errors) {
-            msg.send('errrrrrooooorrrrrrrrrr');
-            msg.send(body.errors);
-            return;
+            results.push('errrrrrooooorrrrrrrrrr');
+            results.push(body.errors);
+            callback(results);
         }
 
         daysUsed = 0;
         // List of submissions contains only most recent submission
         submissions = body[0].submissions;
-        console.log(submissions.length);
         var i = 0, end = submissions.length, subm;
         for(; i < end; i += 1) {
             subm = submissions[i];
 
-            if (subm.late === true) { // late is fale even for no submission!
-                console.log(subm);
+            if (subm.late) { // late is fale even for no submission!
                 days = getSlipDays(subm.submitted_at, subm.assignment.due_at);
                 daysUsed += days;
                 if (days > 0) {
-                    msg.send('Used ' + days.toString() + ' slip days for assignment ' +
+                    results.push('Used ' + days.toString() + ' slip days for assignment ' +
                         subm.assignment.name);
                 }
-                
             }
-
-            console.log('Days Used:');
-            console.log(daysUsed);
-
         }
 
-        msg.send('Total: ' + daysUsed.toString() + ' slip days used.');
+        results.push('Total: ' + daysUsed.toString() + ' slip days used.');
+
+        callback(results);
     });
 }
