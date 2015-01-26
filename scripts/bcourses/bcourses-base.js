ac06bca14565ffd029af9053441ff0e65a30a7dd
Michael Ball <cycomachead@gmail.com>
2015-01-26 00:17:11 -0800

    room command and wip new lab checkoff stuff

--- scripts/bcourses/bcourses-base.js
+++ scripts/bcourses/bcourses-base.js
@@ -2,21 +2,22 @@ var Canvas    = require('node-canvas-lms');
 var authToken = process.env.HUBOT_CANVAS_KEY;
 
 var bCoursesURL = 'https://bcourses.berkeley.edu/';
+
+var cs10 = new Canvas(bCoursesURL, { token: authToken });
+
 // Update Each Semester
-// CS10 FA14: '1246916'
 // Michael Sandbox: '1268501'
-var cs10CourseID = '1246916';
+// var cs10CourseID = '1301472'; Spring 2015
+cs10.courseID = '1301472';
 // Update Each Semester
-// CS10 FA14 Labs 1549984
 // Michael Sandbox: 1593713
-var labsAssnID = '1549984';
-// Make sure only a few people can assign grades
-// TODO: Grab the actual strings from HipChat
-// We can also use the "secret" room...
-var allowedRooms = ['lab_check-off_room', 'cs10_staff_room_(private)'] + [ process.env.HUBOT_SECRET_ROOM ];
+// cs10.labsID = '1702126' Spring 2015
+cs10.labsID = '1702126';
+
+cs10.uid = 'sis_user_id:';
 
 // Mapping of extenstion IDs to bCourses IDs
-var SWAP_IDS = {
+cs10.SWAP_IDS = {
     '539182':'UID:1083827',
     '538761':'UID:1074257',
     '538594':'UID:1074141',
@@ -24,6 +25,8 @@ var SWAP_IDS = {
     '539072':'UID:1082812',
 };
 
-
+cs10.normalizeID = function(sid) {
+    
+}
 
 module.exports = cs10;
