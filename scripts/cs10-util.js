5d8729a3f1fee973153fb3d4863e4676f4e5642e
Michael Ball <cycomachead@gmail.com>
2015-02-05 21:11:16 -0800

    new ta links command; cleanup query stuff

---
+++ scripts/cs10-util.js
@@ -0,0 +1,22 @@
+// Description:
+//  Simple stuff for TAs
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   hubot links | forms -- show useful TA notes.
+//
+// Author:
+//  Michael Ball
+
+module.exports = function(robot) {
+    
+    robot.respond(/.*(links|forms).*/, function(msg) {
+        msg.send('Late Assignments Form: http://bjc.link/sp15lateassignment');
+        msg.send()
+    });
+}
