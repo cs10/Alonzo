f2e52288c5d116da594fc3681a7964d6270b3404
Michael Ball <cycomachead@gmail.com>
2015-02-07 22:45:56 -0800

    wip LA review

--- scripts/cs10-util.js
+++ scripts/cs10-util.js
@@ -14,7 +14,7 @@
 //  Michael Ball
 
 module.exports = function(robot) {
-    
+
     robot.respond(/.*(links|forms).*/, function(msg) {
         msg.send('Late Assignments Form: http://bjc.link/sp15lateassignment');
         msg.send()
