3400a90bf6dbda73e71472e06b2e6e19e2970da3
Michael Ball <cycomachead@gmail.com>
2015-02-10 00:32:52 -0800

    some commenting and README retooling

--- scripts/cs10-util.js
+++ scripts/cs10-util.js
@@ -9,14 +9,25 @@
 //
 // Commands:
 //   hubot links | forms -- show useful TA notes.
+//   hubot locker combo? -- show the locker comber for lab
 //
 // Author:
 //  Michael Ball
 
+cs10 = require('./bcourses/');
+
 module.exports = function(robot) {
 
-    robot.respond(/.*(links|forms).*/, function(msg) {
-        msg.send('Late Assignments Form: http://bjc.link/sp15lateassignment');
-        msg.send()
+    robot.respond(/.*(links|forms).*/i, function(msg) {
+        var txt = '';
+        txt += 'Late Assignments Form: http://bjc.link/sp15lateassignment\n';
+        txt += 'Grade book:' + cs10.gradebookURL + '\n';
+        txt += 'Contacts Sheet: http://bjc.link/cs10contacts\n';
+        msg.send(txt)
     });
+    
+    robot.respond( /\s*locker( combo)?/i, function(msg) {
+        msg.send('11-29-15');
+    });
+    
 }
