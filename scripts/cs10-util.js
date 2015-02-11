d8c59a7a8651c109930c03be9b81503e18d513fc
Michael Ball <cycomachead@gmail.com>
2015-02-10 16:19:05 -0800

    LA attendance link

--- scripts/cs10-util.js
+++ scripts/cs10-util.js
@@ -21,6 +21,7 @@ module.exports = function(robot) {
     robot.respond(/.*(links|forms).*/i, function(msg) {
         var txt = '';
         txt += 'Late Assignments Form: http://bjc.link/sp15lateassignment\n';
+        txt += 'LA Attendance: https://bcourses.berkeley.edu/courses/1301477/external_tools/36957\n';
         txt += 'Grade book:' + cs10.gradebookURL + '\n';
         txt += 'Contacts Sheet: http://bjc.link/cs10contacts\n';
         msg.send(txt)
