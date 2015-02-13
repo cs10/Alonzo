ec0f523fa063706782e73f6077416d94bd41bcf7
Michael Ball <cycomachead@gmail.com>
2015-02-13 13:51:22 -0800

    init empty express server

---
+++ scripts/server.js
@@ -0,0 +1,20 @@
+// Description:
+//   Setup using the chatbot as a slightly more generic web server
+//
+// Dependencies:
+//   None, at the moment.
+//
+// Configuration:
+//   EXPRESS_STATIC = the directory to serve files from
+//
+//
+// Author:
+//  Michael Ball
+
+module.exports = function(robot) {
+    // var express = require('express');
+    // var app = robot.router;
+    //
+    // app.use(express.static(__dirname + '/public'));
+
+}
