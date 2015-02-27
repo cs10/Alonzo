2e2c8a256b40b0e8545d3b8c67f4aa7039ba30f5
Michael Ball <cycomachead@gmail.com>
2015-02-26 23:20:58 -0800

    spacing...

--- scripts/cs10-slipdays.js
+++ scripts/cs10-slipdays.js
@@ -64,12 +64,12 @@ function getSlipDays(submissionTime, dueTime) {
 **/
 var STATE_GRADED = 'graded';
 function calculateSlipDays(sid, callback) {
-    var url = cs10.baseURL + 'students/submissions',
+    var url     = cs10.baseURL + 'students/submissions',
         options = {
-        'include[]' : ['assignment', 'submission_comments'],
-        'student_ids[]' : cs10.normalizeSID(sid),
-        'assignment_ids[]' : cs10.slipDayAssignmentIDs
-    };
+            'include[]' : ['assignment', 'submission_comments'],
+            'student_ids[]' : cs10.normalizeSID(sid),
+            'assignment_ids[]' : cs10.slipDayAssignmentIDs
+        };
 
     cs10.get(url, options, function(error, response, body) {
         var days, verified, submitted,
