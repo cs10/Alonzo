20505ec9a8959b6d86a3f3b4003523d8458ef8d5
Michael Ball <cycomachead@gmail.com>
2015-05-14 16:03:47 -0700

    some progress-ish on snap page

---
+++ scripts/snap-proj.js
@@ -0,0 +1,35 @@
+
+// Description:
+//   Setup using the chatbot as a slightly more generic web server
+//
+// Dependencies:
+//   zip
+//   request
+//
+// Configuration:
+//
+//
+// Author:
+//  Michael Ball
+
+var request = require('request');
+var url = require('url');
+var query = require('querystring');
+
+module.exports = function(robot) {
+    robot.router.get('/download-project', function(req, res) {
+        var baseURL = 'https://snap.apps.miosoft.com/SnapCloudPublic?';
+        var project = url.parse(request.url).query;
+        
+        
+        // Get file from the URL query
+        // Parse the encoded response
+        // zip the project-text
+        // respond w/ correct headers
+        
+        request.get(baseURL + url, function(error, response, body) {
+            
+        });
+        
+    });
+}
