bc2fa1d790ccecc0311938ec68ef3f54009446dd
Michael Ball <cycomachead@gmail.com>
2015-06-23 04:20:49 -0700

    OMG THE SNAP DOWNLOADER WORKS!! YAYAYAYYA!

--- scripts/snap-proj.js
+++ scripts/snap-proj.js
@@ -13,22 +13,34 @@
 
 var request = require('request');
 var url = require('url');
-var query = require('querystring');
+var qs = require('querystring');
+var zip = require('node-native-zip');
 
 module.exports = function(robot) {
     robot.router.get('/download-project', function(req, res) {
         var baseURL = 'https://snap.apps.miosoft.com/SnapCloudPublic?';
-        var project = url.parse(request.url).query;
-        
-        
+        var query = url.parse(req.url).query;
+        var qObj = qs.parse(query);
+
         // Get file from the URL query
         // Parse the encoded response
         // zip the project-text
         // respond w/ correct headers
-        
-        request.get(baseURL + url, function(error, response, body) {
-            
+
+        request.get(baseURL + query, function(error, response, body) {
+            var data = qs.parse(body);
+            var fileName = qObj['Username'] + '--' + data['ProjectName'] +
+            '--' + data['Updated'];
+            var archive = new zip();
+            archive.add(fileName + '.xml', new Buffer(data['SourceCode'], 'utf-8'));
+            res.setHeader('Content-disposition',
+                          'attachment; filename=' + fileName + '.zip');
+            res.type('application/zip');
+            var buff = archive.toBuffer();
+            res.setHeader('Content-Length', buff.length);
+            res.write(buff);
+            res.status(200).end();
         });
-        
+
     });
 }
