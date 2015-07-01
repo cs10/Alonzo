4ce51f520e5d3e0072f7a6ed385d2c2e69a265ec
Michael Ball <cycomachead@gmail.com>
2015-07-01 05:28:32 -0700

    Do a lot of refactoring for lab checkoffs

--- scripts/cs10-checkoffs-new.js
+++ scripts/cs10-checkoffs-new.js
@@ -54,22 +54,20 @@ module.exports = function(robot) {
 };
 
 function processCheckOff(msg) {
-    var roomFn, parsed, errors, logPlace;
+    var roomFn, parsed, errors;
     switch (msg.message.room) {
     case LA_ROOM:
         roomFn = doLACheckoff;
-        logPlace = LA_BASE_KEY;
         break;
     case 'Shell': // Move this condition around for command line testing
     case TA_ROOM:
         roomFn = doTACheckoff;
-        logPlace = TA_BASE_KEY;
         break;
     default:
         // don't send a warning message if the user likely wasn't trying
         // to check anyone off.
         if (msg.message.text.match(containsSIDExp)) {
-            msg.send('Lab Check offs are not allowed from this room');
+            msg.reply('Lab Check offs are not allowed from this room');
         }
         return;
     }
@@ -77,96 +75,41 @@ function processCheckOff(msg) {
     parsed = extractMessage(msg.message.text);
     errors = verifyErrors(parsed);
     if (errors.length) {
-        msg.send('Your check off was NOT saved!',
+        msg.reply('Your check off was NOT saved!',
                  'ERROR: The following errors occurred.',
                  'Please check what you typed or ask for help.',
                  errors.join('\n'));
+
+         auditLog(CHECKOFF_ERR, msg, parsed);
         return;
     }
-    // Verify Cache Here
+
+    auditLog(CHECKOFF_DATA + parsed.lab, msg, parsed);
+    
     roomFn(parsed, msg);
 }
 
-/* Proccess the regex match into a common formatted object */
-function extractMessage(text) {
-    // Parse the following components out of a message.
-    var findSIDs = containsSIDExp,
-        findLate = /late/i,
-        findLab  = /\d{1,2}/;
-
-    var labNo  = text.match(findLab) || [0],
-        isLate = text.match(findLate) != null,
-        SIDs   = text.match(findSIDs);
-
-    SIDs = SIDs.map(cs10.normalizeSID);
-
-    return {
-        lab: labNo[0],
-        sids: SIDs,
-        isLate: isLate
-    };
-}
-
 /**
- *  Store the parsed message in the brain so it can be audited if need be.
- *  Audits will be most necessary for "sketchy" lab check-offs.
- *  @param {string} key - the location in the brain to store data.
- *  @param {message} msg - Hubot's message object that has all the user info.
- *  @param {object} data - data to be saved in the brain.
+ *
+ *
  */
-function auditLog(key, msg, data) {
-    var toStore = {
-        room: msg.message.room,
-        msg: msg.message.text,
-        user: msg.message.user.name,
-        time: (new Date()).toString(),
-        parsed: data
-    };
-}
+function doCheckoff(msg, data, type) {
+    var isSketchy;
 
-// Return an array of error messages that prevent the checkoff from being saved.
-function verifyErrors(parsed) {
-    var errors = [];
-    if ((parsed.lab < MIN_LAB || parsed.lab > MAX_LAB) && parsed.lab != 42) {
-        errors.push('The lab number: ' + parsed.lab + ' is not a valid lab!');
-        errors.push('Please specify the lab number before all student ids.');
-    }
-    if (parsed.sids.length < 1) {
-        errors.push('No SIDs were found.');
-    }
-
-    return errors;
-}
-
-// Cache
-// TODO: document wacky callback thingy
-function verifyCache(callback, args) {
-    var cached = robot.brain.get(LAB_CACHE_KEY);
-    if (cacheIsValid(cached)) {
-        callback.apply(null, args);
-    } else {
-        cacheLabAssignments(callback, args);
-    }
-}
+    if (isSketchy) {
+        // Do warnings.
+        // We probably want to alert people.
+        msg.reply();
+        msg.send('@headTA');
 
 
-function cacheLabAssignments(callback, args) {
-    var url   = cs10.baseURL + 'assignment_groups/' + cs10.labsID,
-        query = {'include[]': 'assignments'};
-
-    cs10.get(url, query, function(error, response, body) {
-        var assignments = body.assignments;
-        var data = {};
-
-        data.time = (new Date()).toString();
-        data.labs = assignments;
-
-        robot.brain.set(LAB_CACHE_KEY, data);
-
-        if (callback) {
-            callback.apply(null, args);
+        if (type != 'TA') {
+            // TAs are allowed to post sketchy scores...
+            return;
         }
-    });
+    }
+
+    // Post grades.
 }
 
 // FIXME -- protect against infinite loops!!
@@ -209,107 +152,88 @@ function doTACheckoff(data, msg) {
 }
 
 function doLACheckoff(data, msg) {
-    var checkoff = {
-        lab: data.lab,
-        late: data.isLate,
-        sid: data.sids,
-        time: (new Date()).toString(),
-        laname: msg.message.user.name
-    };
 
     var sketchy = isSketchy(checkoff);
     if (sketchy) {
         msg.send('ERROR: You\'re being sketchy right now...\n',
                  sketchy.join('\n'),
                  'This checkoff will not be saved. :(');
-        var LA_DATA = robot.brain.get(LA_DATA_KEY) || [];
-        LA_DATA.push(checkoff);
-        robot.brain.set(LA_DATA_KEY, LA_DATA);
         return;
-    }
+    };
     // Post scores to bCourses
     var scores = 'score' + (data.sids.length === 1 ? '' : 's');
     msg.send('LA: Saved ' + data.sids.length + ' student '+ scores +
              ' for lab ' + data.lab  + '.');
-
 }
 
-function postSignleAssignment(assnID, sid, score, msg) {
-var scoreForm = 'submission[posted_grade]=' + score,
-    url = cs10.baseURL + 'assignments/' + assnID + '/submissions/' + sid;
 
-    cs10.put(url, '', scoreForm, verifyScoreSubmission(sid, score, msg));
-}
+//////////////////////////////////////////////////////////////////////////////
+// Parsing / Validation
+//////////////////////////////////////////////////////////////////////////////
 
-// Error Handler for posting lab check off scores.
-function verifyScoreSubmission(sid, points, msg) {
-    return function(error, response, body) {
-        var errorMsg = 'Problem encountered for ID: ' + sid.replace(cs10.uid, '');
-        if (body.errors || !body.grade || body.grade != points.toString()) {
-            failures += 1;
-            if (body.errors && body.errors[0]) {
-                errorMsg += '\nERROR:\t' + body.errors[0].message;
-            }
-            errorMsg += '\n' + 'Please enter the score directly in bCoureses.';
-            errorMsg += '\n' + cs10.gradebookURL;
-            msg.send(errorMsg);
-        } else {
-            successes += 1;
-        }
-        if (successes + failures === expectedScores) {
-            clearTimeout(timeoutID);
-            if (successes) {
-                var scores = successes + ' score' + (successes == 1 ? '' : 's');
-                msg.send(scores + ' successfully updated.');
-            }
-            if (failures) {
-                msg.send('WARING: ' + failures + ' submissions failed.');
-            }
-        }
+/**
+ *  Parse the message into a common formatted object.
+ *  @param {string} text - message text.
+ */
+function extractMessage(text) {
+    // Parse the following components out of a message.
+    var findSIDs = containsSIDExp,
+        findLate = /late/i,
+        findLab  = /\d{1,2}/;
+
+    var labNo  = text.match(findLab) || [0],
+        isLate = text.match(findLate) != null,
+        SIDs   = text.match(findSIDs);
+
+    SIDs = SIDs.map(cs10.normalizeSID);
+
+    return {
+        lab: labNo[0],
+        sids: SIDs,
+        isLate: isLate
     };
 }
 
-function cacheIsValid(assignments) {
-    var labsExist = assignments.labs ? assignments.labs.length > 0 : false;
-    var date = assignments.time;
-    var diff = (new Date()) - (new Date(date));
-    return labsExist && diff / (1000 * 60 * 60) < CACHE_HOURS;
+/**
+ *  Store the parsed message in the brain so it can be audited if need be.
+ *  Audits will be most necessary for "sketchy" lab check-offs.
+ *  @param {string} key - the location in the brain to store data.
+ *  @param {message} msg - Hubot's message object that has all the user info.
+ *  @param {object} data - data to be saved in the brain.
+ */
+function auditLog(key, msg, data) {
+    var toStore = {
+        room: msg.message.room,
+        msg: msg.message.text,
+        user: msg.message.user.name,
+        time: (new Date()).toString(),
+    };
+    for (key in data) {
+        toStore[key] = data[key];
+    }
+    
+    var brainData = robot.brain.get(key) || [];
+    brainData.push(toStore);
+    robot.brain.set(key, brainData);
 }
 
+// Return an array of error messages that prevent the checkoff from being saved.
+function verifyErrors(parsed) {
+    var errors = [];
+    if ((parsed.lab < MIN_LAB || parsed.lab > MAX_LAB) && parsed.lab != 42) {
+        errors.push('The lab number: ' + parsed.lab + ' is not a valid lab!');
+        errors.push('Please specify the lab number before all student ids.');
+    }
+    if (parsed.sids.length < 1) {
+        errors.push('No SIDs were found.');
+    }
 
-// Return the bCourses lab object matching the CS10 lab number
-// All labs are named "<#>. <Lab Title>"
-function findLabByNum(num, labs) {
-    var result;
-    labs.some(function(lab) {
-        var labNo = lab.name.match(/^(\d{1,2})/);
-        if (labNo[1] == num) {
-            result = lab;
-            return true;
-        }
-        return false;
-    });
-    return result || { id: 0 };
+    return errors;
 }
 
-function getAssignmentID(num, assignments) {
-    var lab = findLabByNum(num, assignments.labs);
-    return lab.id || false;
-}
-
-
-function sendLAStats(ladata, msg) {
-    var safe = getSIDCount(ladata.safe);
-    var text = 'LA Data Processed:\n';
-    text += 'Found Safe Check offs for: ' + Object.keys(ladata.safe).join(' ') +
-            ' labs.\n';
-    text += 'Found Sketchy Check offs for: ' +
-            (Object.keys(ladata.sketchy.labs).join(' ') || 'no') + ' labs.\n';
-    text += 'Total of ' + safe.ontime + ' good on time checkoffs, ' +
-            safe.late + ' late check offs.\n';
-    msg.send(text);
-}
-
+//////////////////////////////////////////////////////////////////////////////
+// Verifying Sketchiness
+//////////////////////////////////////////////////////////////////////////////
 
 /** Determine whether an LA checkoff is sketchy.
     "Sketchy" means: More than 1 week paste the due date,
@@ -370,3 +294,108 @@ var sketchyTests = {
         message: ''
     }
 };
+
+//////////////////////////////////////////////////////////////////////////////
+// Posting Scores
+//////////////////////////////////////////////////////////////////////////////
+
+function postSignleAssignment(assnID, sid, score, msg) {
+var scoreForm = 'submission[posted_grade]=' + score,
+    url = cs10.baseURL + 'assignments/' + assnID + '/submissions/' + sid;
+
+    cs10.put(url, '', scoreForm, verifyScoreSubmission(sid, score, msg));
+}
+
+// Error Handler for posting lab check off scores.
+function verifyScoreSubmission(sid, points, msg) {
+    return function(error, response, body) {
+        var errorMsg = 'Problem encountered for ID: ' + sid.replace(cs10.uid, '');
+        if (body.errors || !body.grade || body.grade != points.toString()) {
+            failures += 1;
+            if (body.errors && body.errors[0]) {
+                errorMsg += '\nERROR:\t' + body.errors[0].message;
+            }
+            errorMsg += '\n' + 'Please enter the score directly in bCourses.';
+            errorMsg += '\n' + cs10.gradebookURL;
+            msg.send(errorMsg);
+        } else {
+            successes += 1;
+        }
+        if (successes + failures === expectedScores) {
+            clearTimeout(timeoutID);
+            if (successes) {
+                var scores = successes + ' score' + (successes == 1 ? '' : 's');
+                msg.send(scores + ' successfully updated.');
+            }
+            if (failures) {
+                msg.send('WARING: ' + failures + ' submissions failed.');
+            }
+        }
+    };
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+// Utility
+//////////////////////////////////////////////////////////////////////////////
+
+// Return the bCourses lab object matching the CS10 lab number
+// All labs are named "<#>. <Lab Title>"
+function findLabByNum(num, labs) {
+    var result;
+    labs.some(function(lab) {
+        var labNo = lab.name.match(/^(\d{1,2})/);
+        if (labNo[1] == num) {
+            result = lab;
+            return true;
+        }
+        return false;
+    });
+    return result || { id: 0 };
+}
+
+function getAssignmentID(num, assignments) {
+    var lab = findLabByNum(num, assignments.labs);
+    return lab.id || false;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// Caching
+//////////////////////////////////////////////////////////////////////////////
+
+// TODO: document wacky callback thingy
+function verifyCache(callback, args) {
+    var cached = robot.brain.get(LAB_CACHE_KEY);
+    if (cacheIsValid(cached)) {
+        callback.apply(null, args);
+    } else {
+        cacheLabAssignments(callback, args);
+    }
+}
+
+
+function cacheLabAssignments(callback, args) {
+    var url   = cs10.baseURL + 'assignment_groups/' + cs10.labsID,
+        query = {'include[]': 'assignments'};
+
+    cs10.get(url, query, function(error, response, body) {
+        var assignments = body.assignments;
+        var data = {};
+
+        data.time = (new Date()).toString();
+        data.labs = assignments;
+
+        robot.brain.set(LAB_CACHE_KEY, data);
+
+        if (callback) {
+            callback.apply(null, args);
+        }
+    });
+}
+
+function cacheIsValid(assignments) {
+    var labsExist = assignments.labs ? assignments.labs.length > 0 : false;
+    var date = assignments.time;
+    var diff = (new Date()) - (new Date(date));
+    return labsExist && diff / (1000 * 60 * 60) < CACHE_HOURS;
+}
