7e5cc025ab433a4e137b76e9776b6b40d7c1a037
Michael Ball <cycomachead@gmail.com>
2015-07-04 17:32:10 -0700

    Finish the edX enrollment function

--- scripts/edx.js
+++ scripts/edx.js
@@ -13,19 +13,31 @@
 // Author:
 //  Michael Ball
 
+var edXAPI = 'https://bjcx-api.herokuapp.com';
+
 module.exports = function(robot) {
     var courseIDs = [
-        
+        'BerkeleyX/BJC.1x/3T2015',
+        'BerkeleyX/BJC.2x/3T2015',
+        'BerkeleyX/BJC.3x/1T2016',
+        'BerkeleyX/BJC.4x/1T2016'
     ];
-    var edXAPI = 'https://bjcx-api.herokuapp.com/course-enrollment-total?course=BerkeleyX/BJC.1x/3T2015&course=BerkeleyX/BJC.2x/3T2015&course=BerkeleyX/BJC.3x/1T2016&course=BerkeleyX/BJC.4x/1T2016';
+    var query = 'course=' + courseIDs.join('&course=');
+    var endpoint = '/course-enrollment-total?' + query;
     robot.respond(/edx\s+(?:enrollment\s*)?update/, function(msg) {
         msg.send('Checking BJCx Enrollment')
-        robot.http(edXAPI).get()(function(err, resp, body) {
+        robot.http(edXAPI + endpoint).get()(function(err, resp, body) {
             if (err) {
                 msg.reply('Uh, oh! An error occurred: ', err);
                 return;
             }
-            console.log(body);
-        })
-    })
-}
+            body = JSON.parse(body);
+            msg.send('Here are the latest enrollment numbers:')
+            courseIDs.forEach(function(id) {
+                var short = id.split('/')[1];
+                msg.send(short + ':\t' + body[id]);
+            });
+        });
+    });
+
+};
