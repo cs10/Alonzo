b869e9afc900118f51242103aadc2a555e6d1c0a
Michael Ball <cycomachead@gmail.com>
2015-08-27 02:07:33 -0700

    add middleware start

---
+++ scripts/authorization.js
@@ -0,0 +1,14 @@
+//  DESCRIPTION:
+//  CS10 Authorization Framework for Chat Commands
+//
+//  This sets up a generic way to restrict access to commands to TAs only.
+//  Based on "middleware" which is executed between after a command is matched
+//  but before the function is executed. See the following docs.
+// https://github.com/github/hubot/blob/master/docs/patterns.md#restricting-access-to-commands
+
+
+module.exports = function (robot) {
+    // robot.listenerMiddleware (function (context, next, done) {
+    //     next();
+    // });
+}
