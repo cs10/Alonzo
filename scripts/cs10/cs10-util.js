0812c0a8f71c826ba4d5afe39bea27847672ebf6
Michael Ball <cycomachead@gmail.com>
2015-09-22 19:49:07 -0700

    Fix links for realz

--- scripts/cs10/cs10-util.js
+++ scripts/cs10/cs10-util.js
@@ -19,8 +19,8 @@ cs10 = require('./bcourses/');
 module.exports = function(robot) {
     robot.respond(/.*(links|forms).*/i, {id: 'cs10.links'}, function(msg) {
         var txt = '';
-        txt += 'Late Assignments Form: http://bjc.link/sp15lateassignment\n';
-        txt += 'LA Attendance: https://bcourses.berkeley.edu/courses/1301477/external_tools/36957\n';
+        txt += 'Late Assignments Form: http://bjc.link/lateformfa15\n';
+        // txt += 'LA Attendance: https://bcourses.berkeley.edu/courses/1301477/external_tools/36957\n';
         txt += 'Grade book:' + cs10.gradebookURL + '\n';
         txt += 'Checkoff Answers: http://bjc.link/cs10checkoffquestions\n';
         txt += 'Contacts Sheet: http://bjc.link/cs10contacts\n';
