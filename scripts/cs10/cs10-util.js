83e733ef6d680a05e71c66098c4ba8d5edd9426c
Michael Ball <cycomachead@gmail.com>
2015-11-15 15:33:24 -0800

    Refactor Links function, add project downloader.

--- scripts/cs10/cs10-util.js
+++ scripts/cs10/cs10-util.js
@@ -18,13 +18,15 @@ cs10 = require('./bcourses/');
 
 module.exports = function(robot) {
     robot.respond(/.*(links|forms).*/i, {id: 'cs10.links'}, function(msg) {
-        var txt = '';
-        txt += 'Late Assignments Form: http://bjc.link/lateformfa15\n';
-        // txt += 'LA Attendance: https://bcourses.berkeley.edu/courses/1301477/external_tools/36957\n';
-        txt += 'Grade book:' + cs10.gradebookURL + '\n';
-        txt += 'Checkoff Answers: http://bjc.link/cs10checkoffquestions\n';
-        txt += 'Contacts Sheet: http://bjc.link/cs10contacts\n';
-        msg.send(txt);
+        var txt = [
+            'Late Assignments Form: http://bjc.link/lateformfa15',
+            'Grade book:' + cs10.gradebookURL + '',
+            'Checkoff Answers: http://bjc.link/cs10checkoffquestions',
+            'Contacts Sheet: http://bjc.link/cs10contacts',
+            'Get Snap! Project: https://alonzo.herokuapp.com/snap-proj.html'
+        ];
+        // 'LA Attendance: https://bcourses.berkeley.edu/courses/1301477/external_tools/36957';
+        msg.send(txt.join('\n'));
     });
 
     robot.respond(/\s*locker( combo)?/i, {id: 'cs10.locker'}, function(msg) {
