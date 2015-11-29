87aa451abff09f3ededa05ad2a0174916dddf061
Michael Ball <cycomachead@gmail.com>
2015-11-28 23:54:51 -0800

    some random cleanup-y stuff

--- scripts/fun/staff-sass.js
+++ scripts/fun/staff-sass.js
@@ -1,22 +1,23 @@
-// Description:
+// DESCRIPTION:
 //   Our staff is too cheerful. They need to be sassed on occassion.
 //   Everything here has only the intention of being funny,
 //   so try to relax a little if you feel mad bro ;)
 //
-// Dependencies:
-//   N/A
+// DEPENDENCIES:
+//   None
 //
-// Configuration:
-//   N/A
+// CONFIGURATION:
+//   None
 //
-// Commands:
-//   hubot <name> is a noob -- responds with the links for the chatbot commands
+// COMMANDS:
+//   hubot <name> is a noob — responds with links to the chatbot commands
 //
-//  Listeners:
+// LISTENERS:
 //    any words appearing in sheSaidKeywords will trigger a response
 //
-// Author:
+// AUTHOR:
 //  Andrew Schmitt
+//  Michael Ball
 
 module.exports = function (robot) {
 
@@ -25,17 +26,21 @@ module.exports = function (robot) {
     }, function(resp) {
         var name = resp.match[1];
         resp.send(`@${name} you should check out the collection of chatbot commands:`);
+        // TODO: Config this URL?
         resp.send('http://alonzo.herokuapp.com/Alonzo/help');
         resp.send(`So you can stop being such a n${msg.match[3]}b....`);
     });
 
-    //add more terms here for alonzo to respond with
+    // Terms used for the bot to respond with
     // '@user That's what she said'
-    var sheSaidKeywords = ['huge', 'giant', 'girth',
+    var sheSaidKeywords = [
+        'huge', 'giant', 'girth',
         'diameter', 'pound', 'enormous', 'massive',
         'fast(er)?', 'push', 'fist', 'ride'
-    ];
-    robot.hear(new RegExp(`.*${sheSaidKeywords.join('|')}`), {
+    ],
+        sheSaidRE = new RegExp(`.*\b${sheSaidKeywords.join('|\b')}`);
+    
+    robot.hear(sheSaidRE, {
         id: 'fun.staff-sass.what-she-said'
     }, function(resp) {
         if (resp.message.user.mention_name) {
@@ -44,8 +49,9 @@ module.exports = function (robot) {
     });
 };
 
-// Flips a coin to determine whether that's what she said should have the message:
-// "And he respected it!" appended to the end. This is not meant to be offensive. Just funny
+// Flips a coin to determine whether that's what she said should have the
+//  message: "And he respected it!" appended to the end.
+//  This is not meant to be offensive. Just funny
 function makeFeminist() {
     return (Math.random() < 0.5) ? '' : 'And he respected it!';
 }
