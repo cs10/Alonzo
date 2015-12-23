50745bb385f2854101553f7c67d86616ec5b4a50
Travis CI <aschmitt@berkeley.edu>
2015-12-23 02:02:58 -0500

    forgot to remove old code

--- scripts/fun/staff-sass.js
+++ scripts/fun/staff-sass.js
@@ -12,9 +12,6 @@
 // COMMANDS:
 //   hubot <name> is a noob — responds with links to the chatbot commands
 //
-// LISTENERS:
-//    any words appearing in sheSaidKeywords will trigger a response
-//
 // AUTHOR:
 //  Andrew Schmitt
 //  Michael Ball
@@ -30,33 +27,4 @@ module.exports = function (robot) {
         resp.send('http://alonzo.herokuapp.com/Alonzo/help');
         resp.send(`So you can stop being such a n${msg.match[3]}b....`);
     });
-
-    // Terms used for the bot to respond with
-    // '@user That's what she said'
-    var sheSaidKeywords = [
-        'huge', 'giant', 'girth',
-        'diameter', 'pound', 'enormous', 'massive',
-        'fast(er)?', 'push', 'fist', 'ride'
-    ],
-        sheSaidRE = new RegExp(`.*\b${sheSaidKeywords.join('|\b')}`);
-
-    robot.hear(sheSaidRE, {
-        id: 'fun.staff-sass.what-she-said'
-    }, function(resp) {
-        if (resp.message.user.mention_name) {
-            resp.send(`@${resp.message.user.mention_name} that's what she said... ${makeFeminist()}`);
-        }
-    });
-    
-    robot.respond(/stfu/i, {id: 'stfu'}, function(resp) {
-        resp.send('Sorry, you should yell @Andy. Not my fault. :(',
-                    'I\'m, just programmed poorly.');
-    })
 };
-
-// Flips a coin to determine whether that's what she said should have the
-//  message: "And he respected it!" appended to the end.
-//  This is not meant to be offensive. Just funny
-function makeFeminist() {
-    return (Math.random() < 0.5) ? '' : 'And he respected it!';
-}
