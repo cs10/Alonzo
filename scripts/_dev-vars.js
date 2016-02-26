956cfd6524bd21c4427fb7091914107dad57d488
Michael Ball <cycomachead@gmail.com>
2016-02-25 17:59:38 -0800

    Update env vars script to use `heroku` command

--- scripts/_dev-vars.js
+++ scripts/_dev-vars.js
@@ -7,23 +7,29 @@ function loadFromHeroku() {
     var dotenv = require('dotenv');
 
     try {
+        // Ensure the command exists.
+        child_process.execSync('which heroku');
         // Grab Heroku Data
-        child_process.execSync('hk env -a alonzo > .env');
+        child_process.execSync('heroku config -s -a alonzo > .env');
         // Append public stuff to Heroku data
         child_process.execSync('cat .env.local >> .env');
         dotenv.load();
         console.log('Successfully grabbed env variables from Heroku');
     } catch (e) {
         console.log('Can\'t grab env variables from Heroku.');
-        console.log('See README for directions on installing `hk`.');
         console.log(e);
+        console.log('See README for directions on installing `heroku`.');
         console.log('');
         // local (committed) variables
         dotenv.load({path: '.env.local'});
-        console.log('Loading Local Files Only');
+        console.log('Loading Local Environments Only...');
     }
 }
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     loadFromHeroku();
+}
+
+module.exports = function (robot) {
+    // empty function. This kills WARNING logs.
 }
