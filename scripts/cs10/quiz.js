28d973ade68c6f92431a7ceefb540e613efcc845
Lara McConnaughey <laram@berkeley.edu>
2016-06-22 09:37:45 -0700

    updates staff room to summer staff room

--- scripts/cs10/quiz.js
+++ scripts/cs10/quiz.js
@@ -19,7 +19,7 @@ var crypto = require('crypto');
 var cs10 = require('./bcourses-config.js');
 
 // Resetting a password can only be done in the TA room
-var TA_ROOM = 'cs10_staff_room_(private)',
+var TA_ROOM = 'cs10_staff_room_Su16_(private)',
     RQ_GROUP_NAME = 'Reading Quizzes',
     RESET_MINS = 30,
     TIMEOUT = 1000 * 60 * RESET_MINS,
