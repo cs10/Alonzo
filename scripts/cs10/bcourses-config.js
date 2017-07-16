674113edbaa87b06ec54973df6c2436aba5ce7dd
Lara McConnaughey <laram@berkeley.edu>
2017-07-15 19:38:58 -0700

    Update bcourses-config.js

--- scripts/cs10/bcourses-config.js
+++ scripts/cs10/bcourses-config.js
@@ -136,7 +136,7 @@ cs10.slipDayAssignmentIDs = [
 ];
 
 // LABS
-cs10.labCheckOffPoints = 2;
+cs10.labCheckOffPoints = 3;
 cs10.labCheckOffLatePts = 1;
 var oneWeek = 1000 * 60 * 60 * 24 * 7;
 cs10.labSecsAllowedLate = oneWeek;
