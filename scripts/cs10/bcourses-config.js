26b63f2cf3641400e4c1d7ea7fc8063ec00792cf
emansishah <emansishah@berkeley.edu>
2019-06-24 00:22:53 -0700

    added http in front of note

--- scripts/cs10/bcourses-config.js
+++ scripts/cs10/bcourses-config.js
@@ -85,7 +85,7 @@ cs10.HELP_LINKS = [
   'AI Attendance: http://bjc.link/su19AI',
   'iClicker Checkout (TAs fill out): http://bjc.link/su19iclickercheckout',
   'Student Register iClicker: http://bjc.link/su19iclickerreg',
-  'Late Assignment Grading Request: bjc.link/su19lategrade'
+  'Late Assignment Grading Request: http://bjc.link/su19lategrade'
 ];
 
 // Links to backup sites for when things go to hell
